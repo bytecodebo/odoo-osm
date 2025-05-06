122a050fea6e38ef7634fa31d59d750bee68f2e2
RSV MacMini <robertsvx@gmail.com>
2025-05-06 01:40:53 -0400

    automatic commit at 2025-05-06T05:40:52UTC

--- byc_core_common/__manifest__.py
+++ byc_core_common/__manifest__.py
@@ -2,7 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "BYC Core Common",
-    "summary": "Base module with global classes and functions used in other modules",
+    "summary": """Base module with global classes and functions used in other modules,
+                  Common functions""",
     "version": "16.0.1.0.1",
     "category": "Sales",
     "website": "http://bytecodebo.com",
